40dc169652c3554a8872d7d15cb4e21a8705760b
sepans <sepans@sepans.com>
2014-09-04 16:15:33 -0400

    changes, expect

--- utils/move_repos.sh
+++ utils/move_repos.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-repo='../../../move/thelma-charts'
+repo=~/Projects/thelma_components/development/thelma-charts
+dev_dir=~/Projects/thelma_components/development
+
+GIT_USERNAME='sepans'
 
 #cd $repo
 prefix='th-'
@@ -11,7 +14,7 @@ all_files=(`cd $repo && ls $prefix*`)
 
 count=0
 for i in `find $repo -name $prefix'*.html' -type f`; do
-	#echo $i
+	echo $i
 	if [ $count -gt 0 ] ; then
 		break;
 	fi
@@ -26,12 +29,37 @@ for i in `find $repo -name $prefix'*.html' -type f`; do
 
 	echo $otherFiles
 
-	cd $repo
-	git filter-branch --index-filter `git rm --cached -r --ignore-unmatch  $otherFiles` --prune-empty -- --all
+	# not working
+	#cd $repo
+	#git filter-branch --index-filter `git rm --cached -r --ignore-unmatch  $otherFiles` --prune-empty -- --all
+	
+	cd $dev_dir
+	mkdir $componentName 
+	cd $componentName
 
+	pwd
+
+	
 
 	echo $count
 	count=$((count+1))
 done
 
+/usr/bin/expect << EOF
 
+	pwd
+
+	spawn yo polymer:seed
+
+	expect "What is your GitHub username?"
+	send "$GIT_USERNAME\r"
+
+	expect "What is your element's name:"
+	send "\r"
+
+	expect "Answer:"
+	send "1\r"
+
+	expect eof
+
+EOF
